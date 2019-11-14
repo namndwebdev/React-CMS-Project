020ac069e6e3a5cb4c1578c026274a6940a5eda9
Doan Arsenal <doanarsenal1998@outlook.com>
2019-11-14 10:04:49 +0700

    First Commit

---
+++ src/configs/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { URI } from './config'
+
+axios.defaults.baseURL = URI;
+axios.defaults.headers.common['Authorization'] = ''
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+const axiosPublic = axios.create({
+  baseURL: URI
+});
+
+export {
+  axios,
+  axiosPublic
+}
---
+++ src/configs/config.js
@@ -0,0 +1,3 @@
+const URI = `http://localhost:1337`
+
+export { URI } 
