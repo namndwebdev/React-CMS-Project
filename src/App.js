c6e48f9116f6eb0f3ef3ef8d9fdfcdfe68d1a8f5
Doan Arsenal <doanarsenal1998@outlook.com>
2019-11-14 10:03:35 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import images from './images1.jpg'
+import './App.css'
+import {axios, axiosPublic} from './configs/axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurants: []
+    }
+  }
+
+  getRestaurant = async () => {
+    try {
+      const res = await axiosPublic.get(`/restaurants`)
+      const restaurants = res.data
+      this.setState({ restaurants })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+        <img src={ images } className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bita xin chào các bạn ahihi :v
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="http://localhost:1337/restaurants" target="_blank" rel="noopener noreferrer">Link API</a>
+        <br></br>
+        <button onClick={this.getRestaurant}>Hiển thị API</button>
+        <ul>
+          {
+            this.state.restaurants.map(restaurants => {
+            return <p key={restaurants.id}>ID: {restaurants.id} <br></br> Name: {restaurants.name} <br></br> Description: {restaurants.description}</p>
+            })
+          }
+        </ul>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
